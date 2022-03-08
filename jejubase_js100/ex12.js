05edec3d0c3cbfd0012f83b3656257a11b330056
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-03-08 21:41:07 +0900

    js 100제 11~15번 문제

---
+++ jejubase_js100/ex12.js
@@ -0,0 +1,23 @@
+// 문제12 : 게임 캐릭터 클래스 만들기
+
+// 출력
+// 545 210 10
+// 파이어볼
+
+/* <여기에 class를 작성하세요.> */
+
+class Wizard {
+  constructor(health, mana, armor) {
+    this.health = health;
+    this.mana = mana;
+    this.armor = armor;
+  }
+
+  attack() {
+    console.log('파이어볼');
+  }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
